2802593ce4f7585591ab4da0a1873610f3acd1d6
Juan Campos <juan@jcamposobando.com>
2025-01-01 11:37:29 -0600

    Move logic from view and models to services.py

--- GlobantPokeBerries/models.py
+++ GlobantPokeBerries/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from . import services
-import os
+
 
 class PokeBerry(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -11,19 +10,3 @@ class PokeBerry(models.Model):
     # Kindly refer to https://docs.djangoproject.com/en/5.1/intro/tutorial02/#playing-with-the-api
     def __str__(self):
         return f"{self.id}-{self.name}"
-
-
-# This might not be the best place for this logic, as it does not belong to the model itself
-# By placing it here it runs only once, when the server starts running
-def load_berry_info ():
-    number_of_berries = services.get_number_of_berries()
-    print(f"number of berries:{number_of_berries}")
-    for i in range(1, number_of_berries + 1):
-        # Cool destructurin assigment, buy maybe brittle, check later
-        pokeBerry = PokeBerry(**services.get_berry_info(i))
-        print(pokeBerry)
-        pokeBerry.save()
-
-#Avoid running it on dev, as it would run on every change
-if (os.getenv("DEBUG_VALUE")!="TRUE"):
-    load_berry_info()
--- GlobantPokeBerries/services.py
+++ GlobantPokeBerries/services.py
@@ -1,6 +1,32 @@
 import requests
+from django.db.models import Avg, Min, Max, Variance
+from collections import Counter
+from .models import PokeBerry
+import os
 
 BASE_URL = "https://pokeapi.co/api/v2/berry"
+
+
+def get_growth_time_list():
+    return PokeBerry.objects.values_list("growth_time", flat=True)
+
+
+def get_pokeBerry_name_list():
+    return ",".join(PokeBerry.objects.values_list("name", flat=True))
+
+
+def calc_growth_time_frequencies():
+    return Counter(PokeBerry.objects.values_list("growth_time", flat=True))
+
+
+def calc_pokeBerry_stats():
+    return PokeBerry.objects.all().aggregate(
+        min_growth_time=Min("growth_time"),
+        max_growth_time=Max("growth_time"),
+        variance_growth_time=Variance("growth_time"),
+        avg_growth_time=Avg("growth_time"),
+    )
+
 
 def get_number_of_berries():
     """Gets the number of berries on pokeapi
@@ -26,6 +52,20 @@ def get_berry_info(id):
     r = requests.get(f"{BASE_URL}/{str(id)}")
     berry = r.json()
     return {
-        "id":berry["id"],
-        "name":berry["name"],
-        "growth_time":berry["growth_time"]}
+        "id": berry["id"],
+        "name": berry["name"],
+        "growth_time": berry["growth_time"],
+    }
+
+
+def load_berry_info():
+    number_of_berries = get_number_of_berries()
+    for i in range(1, number_of_berries + 1):
+        pokeBerry = PokeBerry(get_berry_info(i))
+        pokeBerry.save()
+
+
+# It will run only once
+# Avoid running it on dev, as it would run on every change
+if os.getenv("DEBUG_VALUE") != "TRUE":
+    load_berry_info()
